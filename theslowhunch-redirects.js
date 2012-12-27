f68cf45d5ce598f076ac0583fd80f7dbae2719bc
Nick Grossman <nick@wrkng.net>
2012-12-27 16:06:30 -0500

    first pass

---
+++ theslowhunch-redirects.js
@@ -0,0 +1,20 @@
+
+
+// create namespace
+var TSH = {};
+TSH.oldDomain = "theslowhunch.net";
+TSH.newDomain = "wayback.theslowhunch.net";
+
+// function: redirect
+TSH.redirect = function() {
+	
+	if (document.URL.search('/' + TSH.oldDomain + '\/200/')) {
+		var newURL = document.URL.replace(TSH.oldDomain, TSH.newDomain);
+		console.log(newURL);
+	}
+		
+}
+
+// Fire away
+TSH.redirect();
+

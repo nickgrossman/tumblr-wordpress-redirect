bbaae1e834fa27385b73510efde8a89695c24b9c
Nick Grossman <nick@wrkng.net>
2012-12-27 16:19:54 -0500

    add real redirect

--- theslowhunch-redirects.js
+++ theslowhunch-redirects.js
@@ -4,13 +4,15 @@
 var TSH = {};
 TSH.oldDomain = "nickgrossman.tumblr.com";
 TSH.newDomain = "wayback.theslowhunch.net";
+TSH.searchString = TSH.oldDomain + '/20';
 
 // function: redirect
 TSH.redirect = function() {
 	
-	if (document.URL.search(TSH.oldDomain + '/20') != -1) {
+	if (document.URL.search(TSH.searchString) != -1) {
 		var newURL = document.URL.replace(TSH.oldDomain, TSH.newDomain);
-		console.log(newURL);
+		window.location.href = newURL;
+		window.location.reload();
 	}
 	 	
 }
